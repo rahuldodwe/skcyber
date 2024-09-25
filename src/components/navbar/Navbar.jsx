d5eeda0c80f4a666611b407246e949a467cde29a
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-09-25 15:32:35 +0530

    navbar conneted with routers

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,37 +1,107 @@
-import React, { useState } from 'react';
-import './navbar.css';
-import header_logo from '../../assets/logo.png'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./navbar.css";
+import header_logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [homePage, setHomePage] = useState(true);
+  const [aboutPage, setAboutPage] = useState(false);
+  const [jobPage, setJobPage] = useState(false);
+  const [collegePage, setCollegePage] = useState(false);
+  const [servicePage, setServicePage] = useState(false);
 
-	const [homePage, setHomePage] = useState(false);
+  function onHomeActive() {
+    setHomePage(true);
+    setJobPage(false);
+    setCollegePage(false);
+    setServicePage(false);
+    setAboutPage(false);
+  }
+
+  function onGovtActive() {
+    setJobPage(true);
+    setAboutPage(false);
+    setHomePage(false);
+    setCollegePage(false);
+    setServicePage(false);
+  }
+
+  function onServiceActive() {
+    setServicePage(true);
+    setAboutPage(false);
+    setHomePage(false);
+    setJobPage(false);
+    setCollegePage(false);
+  }
+
+  function onCollegeActive() {
+    setCollegePage(true);
+    setHomePage(false);
+    setJobPage(false);
+    setCollegePage(false);
+    setServicePage(false);
+  }
+
+  function onAboutActive() {
+    setAboutPage(true);
+    setHomePage(false);
+    setJobPage(false);
+    setCollegePage(false);
+    setServicePage(false);
+  }
 
   return (
     <div className="main-bar sticky-top">
-		<nav>
-			<input type="checkbox" id="check" />
-			<label htmlFor="check" className="checkbtn">
-				<span></span>
-				<span></span>
-				<span></span>
-			</label>
+      <nav>
+        <input type="checkbox" id="check" />
+        <label htmlFor="check" className="checkbtn">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
 
-			<div className="logo-header">
-				<Link to="/" ><img src={header_logo} /></Link>
-			</div>
+        <div className="logo-header">
+          <Link to="/">
+            <img src={header_logo} />
+          </Link>
+        </div>
 
-			<ul>
-				{/* <li className="active"><Link to="/">Home</Link></li> */}
-				<li className=''><Link to="/">Home</Link></li>
-				<li className=""><Link to="/govt-jobs">Govt-Jobs</Link></li>
-				<li className=""><Link to="/college-notification">College Notification</Link></li>
-				<li className=""><Link to="/our-services">Our Services</Link></li>
-				<li className=""><Link to="/about-us">About Us</Link></li>
-			</ul>
-		</nav>
-	</div>
-  )
-}
+        <ul>
+          {/* <li className="active"><Link to="/">Home</Link></li> */}
+          <li
+            className={`${homePage ? "active" : ""}`}
+            onClick={() => onHomeActive()}
+          >
+            <Link to="/">Home</Link>
+          </li>
+          <li
+            className={`${jobPage ? "active" : ""}`}
+            onClick={() => onGovtActive()}
+          >
+            <Link to="/govt-jobs">Govt-Jobs</Link>
+          </li>
+          <li
+            className={`${collegePage ? "active" : ""}`}
+            onClick={() => onCollegeActive()}
+          >
+            <Link to="/college-notification">College Notification</Link>
+          </li>
+          <li
+            className={`${servicePage ? "active" : ""}`}
+            onClick={() => onServiceActive()}
+          >
+            <Link to="/our-services">Our Services</Link>
+          </li>
+          <li
+            className={`${aboutPage ? "active" : ""}`}
+            onClick={() => onAboutActive()}
+          >
+            <Link to="/about-us">About Us</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
