79cb8b09bb9584581a2aa39e581bc1714acab92c
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-10-21 16:07:30 +0530

    Latest Jobs -api setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/home/Home";
@@ -8,8 +8,7 @@ import AboutUS from "./pages/about-us/AboutUS";
 import CollegeNotification from "./pages/college-notification/CollegeNotification";
 import GovtJobs from "./pages/govt-jobs/GovtJobs";
 import OurServices from "./pages/our-services/OurServices";
-
-
+import { API_KEY } from "./data";
 
 const router = createBrowserRouter([
   {
@@ -41,7 +40,33 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router}/>
+  const [latestJob, setlatestJob] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchJobList() {
+    const url = "https://sarkari-result.p.rapidapi.com/scrape/latestjob";
+    const options = {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "ab6fffcdffmshe20aa9437e25573p12c572jsn16ca5b66f527",
+        "x-rapidapi-host": "sarkari-result.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchJobList();
+  }, []);
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;
