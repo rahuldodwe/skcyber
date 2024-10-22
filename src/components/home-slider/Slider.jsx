832935d5fbad066f8439822a040bf66edab50a6a
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-10-22 22:22:51 +0530

    slider setup done

--- src/components/home-slider/Slider.jsx
+++ src/components/home-slider/Slider.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { CAlert, CCarousel, CCarouselCaption, CCarouselItem, CImage } from "@coreui/react";
 import "@coreui/coreui/dist/css/coreui.min.css";
-import slide1 from '../../assets/sk-pic1.jpg'
+import slider2 from '../../assets/sk-pic1.jpg'
+import slider1 from '../../assets/cpct0.jpg'
+import slider3 from '../../assets/c-basic0.jpg'
 import './slider.css';
 
 const Slider = () => {
@@ -9,22 +11,22 @@ const Slider = () => {
     <div className="wid">
       <CCarousel controls indicators>
         <CCarouselItem slider-img >
-          <CImage className="d-block w-100 slider-img" src={slide1} alt="slide 1" />
-          <CCarouselCaption className="d-none d-md-block">
+          <CImage className="d-block w-100 slider-img" src={slider1} alt="slide 1" />
+          <CCarouselCaption className="cap">
             <h5>First slide label</h5>
             <p>Some representative placeholder content for the first slide.</p>
           </CCarouselCaption>
         </CCarouselItem>
         <CCarouselItem slider-img>
-          <CImage className="d-block w-100  slider-img" src={slide1} alt="slide 2" />
-          <CCarouselCaption className="d-none d-md-block">
+          <CImage className="d-block w-100  slider-img" src={slider2} alt="slide 2" />
+          <CCarouselCaption className="cap">
             <h5>Second slide label</h5>
             <p>Some representative placeholder content for the first slide.</p>
           </CCarouselCaption>
         </CCarouselItem>
         <CCarouselItem slider-img>
-          <CImage className="d-block w-100 slider-img" src={slide1} alt="slide 3" />
-          <CCarouselCaption className="d-none d-md-block">
+          <CImage className="d-block w-100 slider-img" src={slider3} alt="slide 3" />
+          <CCarouselCaption className="cap">
             <h5>Third slide label</h5>
             <p>Some representative placeholder content for the first slide.</p>
           </CCarouselCaption>
